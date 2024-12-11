9f2415001673b824892ca87d94f73d051d1bbc20
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-11 10:39:41 +0700

    added User views

---
+++ resources/views/livewire/users/user-edit.blade.php
@@ -0,0 +1,63 @@
+<div>
+    <!-- ========== title-wrapper start ========== -->
+    <div class="title-wrapper pt-30">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="title mb-30">
+                    <h2>{{ __('Edit User') }}</h2>
+                </div>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- ========== title-wrapper end ========== -->
+
+    <div class="card-styles">
+        <div class="card-style-3 mb-30">
+            <div class="card-content">
+                <div class="mb-4 row button">
+                    <div class="col">
+                        <a href="{{ route('users.index') }}" class="btn btn-primary">
+                            <i class="fas fa-arrow-left pe-1"></i> {{ __('Back') }}
+                        </a>
+                    </div>
+                </div>
+                <div class="mb-4 row contact-form">
+                    <form wire:submit="update">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="mb-3 form-group">
+                                    <label for="name" class="text-black form-label fw-bold">{{ __('Name')
+                                        }}</label>
+                                    <input type="text" name="name" id="name" class="form-control" wire:model='name'>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="mb-3 form-group">
+                                    <label for="email" class="text-black form-label fw-bold">{{ __('Email') }}</label>
+                                    <input type="email" name="email" id="email" class="form-control" wire:model='email'>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="mb-3 form-group">
+                                    <label for="role" class="text-black form-label fw-bold">{{ __('Role')
+                                        }}</label>
+                                    <select name="role" id="role" class="form-control" wire:model='role'>
+                                        <option value="">{{ __('Choose option...') }}</option>
+                                        @foreach ($roles as $role)
+                                        <option value="{{ $role->name }}">{{ $role->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-grid">
+                            <button type="submit" class="text-white btn btn-success">{{ __('Submit') }}</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/livewire/users/user-index.blade.php
@@ -0,0 +1,146 @@
+<div>
+    <!-- ========== title-wrapper start ========== -->
+    <div class="title-wrapper pt-30">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="title mb-30">
+                    <h2>{{ __('Users') }}</h2>
+                </div>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- ========== title-wrapper end ========== -->
+
+    <div class="card-styles">
+        <div class="card-style-3 mb-30">
+            <div class="card-content">
+                <div class="mb-4 row">
+                    <div class="col-lg-6"></div>
+                    <div class="col-lg-6 text-end">
+                        <a href="{{ route('users.create') }}" class="btn btn-success">
+                            <i class="fas fa-plus"></i> {{ __('Create New User') }}
+                        </a>
+                    </div>
+                </div>
+                <div class="row form">
+                    <div class="table-wrapper table-responsive">
+                        <div class="row search">
+                            <div class="col-lg-6">
+                                <input wire:model.live.debounce.250ms='search' type="text" name="search" id="search"
+                                    class="mb-3 form-control w-25" placeholder="Search...">
+                            </div>
+                            <div class="col-lg-6">
+                            </div>
+                        </div>
+                        <table class="table text-center text-black striped-table">
+                            <thead>
+                                <tr>
+                                    <th class="px-2">
+                                        <input type="checkbox" name="selectAll" id="selectAll"
+                                            wire:model.live='selectAll'>
+                                    </th>
+                                    <th style="width: 2em;">No</th>
+                                    <th>{{ __('Name') }}</th>
+                                    <th>{{ __('Email') }}</th>
+                                    <th>{{ __('Role') }}</th>
+                                    <th>{{ __('Action') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if ($users->isEmpty())
+                                <tr>
+                                    <td colspan="5" class="text-center">
+                                        {{ __('Data not found') }}
+                                    </td>
+                                </tr>
+                                @else
+                                @foreach ($users as $user)
+                                <tr>
+                                    <td class="px-2"><input type="checkbox" name="selectedItems" id="selectedItems"
+                                            wire:model.live="selectedItems" value="{{ $user->id }}"></td>
+                                    <td>{{ $users->firstItem() + $loop->index }}</td>
+                                    <td>{{ $user->name ?? '' }}</td>
+                                    <td>{{ $user->email ?? '' }}</td>
+                                    <td>{{ $user->roles->first()->name ?? '' }}</td>
+                                    <td>
+                                        <a href="{{ route('users.edit', $user->userId) }}"
+                                            class="btn btn-primary">
+                                            <i class="align-middle" data-feather="edit"></i> {{ __('Edit') }}
+                                        </a>
+                                        <button class="btn btn-danger"
+                                            wire:click.prevent="deleteConfirm('{{ $user->userId }}')"><i
+                                                class="fas fa-trash pe-1"></i> {{ __('Delete') }}</button>
+                                    </td>
+                                </tr>
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                        <div class="mt-4 row">
+                            <div class="col-lg-6 d-flex align-items-center">
+                                <label class="mb-0 font-bold text-black form-label me-3">Record Per
+                                    Page</label>
+                                <select wire:model.live='perPage' class="text-black form-control per-page"
+                                    style="width: 7%">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="25">25</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-center justify-content-end">
+                                {{ $users->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@script
+<script>
+    window.addEventListener('delete-confirmation', event => {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "User will be deleted permanently!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $wire.dispatch('deleteConfirmed');
+                }
+            });
+        })
+</script>
+@endscript
+@if (session()->has('alert'))
+@script
+<script>
+    const alerts = @json(session()->get('alert'));
+            const title = alerts.title;
+            const icon = alerts.type;
+            const toast = alerts.toast;
+            const position = alerts.position;
+            const timer = alerts.timer;
+            const progbar = alerts.progbar;
+            const confirm = alerts.showConfirmButton;
+
+            Swal.fire({
+                title: title,
+                icon: icon,
+                toast: toast,
+                position: position,
+                timer: timer,
+                timerProgressBar: progbar,
+                showConfirmButton: confirm
+            });
+</script>
+@endscript
+@endif

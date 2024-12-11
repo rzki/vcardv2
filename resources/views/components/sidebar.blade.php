cc7a1c6615cc56e404fd4fc39603346338556372
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-11 09:59:46 +0700

    added active state to menu items

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,12 +1,12 @@
 <nav id="sidebar" class="sidebar js-sidebar">
     <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="index.html">
-            <span class="align-middle">AdminKit</span>
+        <a class="text-center sidebar-brand" href="{{ route('dashboard') }}">
+            <span class="align-middle">{{ env('APP_NAME') }}</span>
         </a>
 
         <ul class="sidebar-nav">
-            <li class="sidebar-item active">
-                <a class="sidebar-link" href="#">
+            <li class="sidebar-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                <a class="sidebar-link" href="{{ route('dashboard') }}">
                     <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
                 </a>
             </li>
